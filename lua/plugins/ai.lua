9e442a6519bec9c885e0ba28ab4ed59686f0b7de
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-09-08 16:07:21 +0630

    feat: add blink cmp for avante

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,46 +3,43 @@ return {
 		"yetone/avante.nvim",
 		event = "VeryLazy",
 		version = false, -- Never set this value to "*"! Never!
-		config = function()
-			require("avante").setup({
-				provider = "copilot",
-				providers = {
-					gemini = {
-						endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-						api_key_name = "GEMINI_API_KEY",
-						model = "gemini-2.5-flash-preview-05-20",
-						timeout = 30000, -- Timeout in milliseconds
-						temperature = 0,
-						max_tokens = 8192,
-					},
+		---@module 'avante'
+		---@type avante.Config
+		opts = {
+			provider = "copilot",
+			auto_suggestions_provider = "copilot",
+			providers = {
+				copilot = { model = "gpt-4.1-2025-04-14" },
+				gemini = {
+					endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+					api_key_name = "GEMINI_API_KEY",
+					model = "gemini-2.5-flash-preview-05-20",
+					timeout = 30000, -- Timeout in milliseconds
+					temperature = 0,
+					max_tokens = 8192,
 				},
-				hints = { enabled = false },
-				windows = { width = 40 },
-
-				-- system_prompt as function ensures LLM always has latest MCP server state
-				-- This is evaluated for every message, even in existing chats
-				system_prompt = function()
-					local hub = require("mcphub").get_hub_instance()
-					return hub and hub:get_active_servers_prompt() or ""
-				end,
-				-- Using function prevents requiring mcphub before it's loaded
-				custom_tools = function()
-					return {
-						require("mcphub.extensions.avante").mcp_tool(),
-					}
-				end,
-			})
-		end,
+			},
+			hints = { enabled = false },
+			windows = { width = 40 },
+			system_prompt = function()
+				local hub = require("mcphub").get_hub_instance()
+				return hub and hub:get_active_servers_prompt() or ""
+			end,
+			-- Using function prevents requiring mcphub before it's loaded
+			custom_tools = function()
+				return {
+					require("mcphub.extensions.avante").mcp_tool(),
+				}
+			end,
+		},
 		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 		build = "make",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
-			"echasnovski/mini.pick", -- for file_selector provider mini.pick
 			"zbirenbaum/copilot.lua", -- for providers='copilot'
 			{
-				-- Make sure to set this up properly if you have lazy=true
 				"MeanderingProgrammer/render-markdown.nvim",
 				opts = {
 					file_types = { "markdown", "Avante" },
@@ -60,6 +57,55 @@ return {
 				end,
 				keys = {
 					{ "<leader>am", ":MCPHub<cr>", desc = "MCP Hub" },
+				},
+			},
+			{
+				"saghen/blink.compat",
+				lazy = true,
+				opts = {},
+				config = function()
+					-- monkeypatch cmp.ConfirmBehavior for Avante
+					require("cmp").ConfirmBehavior = {
+						Insert = "insert",
+						Replace = "replace",
+					}
+				end,
+			},
+			{
+				"saghen/blink.cmp",
+				lazy = true,
+				dependencies = {
+					"Kaiser-Yang/blink-cmp-avante",
+				},
+				opts = {
+					sources = {
+						default = { "avante", "avante_commands", "avante_mentions", "avante_files" },
+						providers = {
+							avante = {
+								module = "blink-cmp-avante",
+								name = "Avante",
+								opts = {},
+							},
+							avante_commands = {
+								name = "avante_commands",
+								module = "blink.compat.source",
+								score_offset = 90, -- show at a higher priority than lsp
+								opts = {},
+							},
+							avante_files = {
+								name = "avante_commands",
+								module = "blink.compat.source",
+								score_offset = 100, -- show at a higher priority than lsp
+								opts = {},
+							},
+							avante_mentions = {
+								name = "avante_mentions",
+								module = "blink.compat.source",
+								score_offset = 1000, -- show at a higher priority than lsp
+								opts = {},
+							},
+						},
+					},
 				},
 			},
 		},
